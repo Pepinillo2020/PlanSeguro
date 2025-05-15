e29c4fd7754226d99eb0c7275fed3f6cc8251d49
k0ddd <130948602+k0ddd@users.noreply.github.com>
2025-05-15 19:31:41 -0400

    Revert "lol"
    This reverts commit 7d172da36bd01a1ff21d07ec72b328793603e4a7.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,8 +5,6 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, Io
 import { RouterLink, RouterLinkActive, Router } from '@angular/router'; // Agregar Router aquí
 import { AuthService } from '../services/auth.service';
 import { HttpClientModule } from '@angular/common/http';  // Importa HttpClientModule
-import { IonIcon } from '@ionic/angular/standalone';
-
 
 @Component({
   selector: 'app-login',
@@ -14,7 +12,7 @@ import { IonIcon } from '@ionic/angular/standalone';
   styleUrls: ['./login.page.scss'],
   standalone: true,
   imports: [
-    IonButton, IonInput, IonItem, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar,
+    IonButton, IonInput, IonItem, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar,
     CommonModule, FormsModule, RouterLink, HttpClientModule // Aquí agregas HttpClientModule
 ]
 })
